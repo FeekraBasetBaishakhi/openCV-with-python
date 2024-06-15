55408bb1cb9d0f703442726050d827a18988f64e
Feekra Baset Baishakhi <65058830+FeekraBasetBaishakhi@users.noreply.github.com>
2024-06-15 18:17:43 +0600

    added code

--- opencvPython/contour.py
+++ opencvPython/contour.py
@@ -2,7 +2,13 @@ import os
 
 import cv2
 
-img = cv2.imread(os.path.join('.',  'data' ,'birds.jpg'))
+img = cv2.imread(os.path.join('.',  'data', 'birds.jpg'))
 
-cv2.threshold(img, )
+ret, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+
+cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE )
+
+cv2.imshow('img', img)
+cv2.imshow('thresh', thresh)
+cv2.waitKey(0)
 
