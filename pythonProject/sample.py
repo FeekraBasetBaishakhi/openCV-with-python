be6b91ef133d56c52fb59c143d68711e62144f9a
Feekra Baset Baishakhi <65058830+FeekraBasetBaishakhi@users.noreply.github.com>
2024-06-24 13:55:01 +0600

    .

--- pythonProject/sample.py
+++
@@ -1,7 +0,0 @@
-import requests
-response = requests.get("https://gitlab.com/api/v4/users/FeekraBasetBaishakhi/projects")
-print(response.text)
-my_projects = response.json()
-
-for project in my_projects:
-    print(f"Project name: {project['name']} \n Project Url: {project['web_url']}\n")
